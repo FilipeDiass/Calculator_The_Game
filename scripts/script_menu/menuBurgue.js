7485243a9fb10675a74c479fe72b9ace681472d4
João Filipe <126769503+FilipeDiass@users.noreply.github.com>
2023-08-18 14:59:16 -0300

    att fix

--- scripts/script_menu/menuBurgue.js
+++ scripts/script_menu/menuBurgue.js
@@ -7,12 +7,14 @@ function menuBurgue(event){
     navbar.classList.toggle('ativo');
     const ativo = navbar.classList.contains('ativo');
     event.currentTarget.setAttribute('aria-expanded', ativo);
+    const body = document.querySelector('body')
     if(ativo === true){
         event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+        body.style.overflow = 'hidden'
     }else{
         event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+        body.style.overflow = 'auto'
     }
-
     //Monitorando o Tamanho da Tela
     window.addEventListener('resize', ()=>{
         if(window.innerWidth >= 950 && window.innerHeight >= 670) navbar.classList.remove('ativo')
