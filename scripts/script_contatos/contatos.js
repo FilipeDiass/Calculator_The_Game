b195de30c276b5a1fdc273945bb8287156895265
João Filipe <126769503+FilipeDiass@users.noreply.github.com>
2023-08-23 13:56:07 -0300

    att fix

--- scripts/script_contatos/contatos.js
+++ scripts/script_contatos/contatos.js
@@ -1,19 +1,28 @@
 const fechar = document.querySelector('#fechar')
 const abrir = document.querySelector('#abrirCont')
+let key = 1
 
 function meuContato(event){
-    if(event.type === 'touch' || event.pointerType === Touch) event.preventDefault();
+    if(event.type === 'touchstart' || event.pointerType === Touch){
+        event.preventDefault();
+    } 
     const container = document.querySelector('#contatos')
     const navbar = document.querySelector('#navbar')
     const body = document.querySelector('body')
-
-    if(container.style.display === 'none'){
+    
+    if(key === 1){
+        container.style.display = 'none'
+    }
+   
+    if(container.style.display == 'none'){
         container.style.display = 'flex'
+        key = 0
     
         if(window.innerWidth >= 950 && window.innerHeight >= 670){
             navbar.classList.remove('ativo')
         }else{
             navbar.classList.toggle('ativo')
+            body.style.overflow = 'hidden'
         } 
     }else{
         container.style.display = 'none'
